82aae404af09b56910e5b710777bb2faee552291
Danya Khrapovitckii <40688428+danyaffff@users.noreply.github.com>
2020-02-29 00:55:49 +0300

    Update main.c

--- Sem 1, Lab 5/main.c
+++ Sem 1, Lab 5/main.c
@@ -1,20 +1,31 @@
-/* Лабораторная работа №5. Массивы */
+// Лабораторная работа №5. Массивы.
+//
+// 1. Заполнить массив значениями согласно варианту. Результат в виде таблицы значений массива вывести на консоль.
+//
+// 2. Реализовать перемножение двух матриц 2х2 на основании данных варианта задания. Результат в виде таблицы значений 
+// элементов результирующей матрицы вывести на консоль.
 
 #include <stdio.h>
 
 int main() {
-    int arr[] = {12, 13, 14, 15, 16, 17, 18, 19, 20};
-    for (int i = 0; i < sizeof(arr) / sizeof(int)+10; i++) {
-        printf("arr[%d] %d\n", i, arr[i]);
+    int array[] = {12, 13, 14, 15, 16, 17, 18, 19, 20};
+    
+    for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        printf("array[%d] = %d\n", i, array[i]);
     }
-    int Matrix1[2][2] = {{1, 1},{0, 1}}, Matrix2[2][2] = {{2, 0}, {3, 0}}, OutputMatrix[2][2];
-    OutputMatrix[0][0] = Matrix1[0][0] * Matrix2[0][0] + Matrix1[0][1] * Matrix2[1][0];
-    OutputMatrix[0][1] = Matrix1[0][0] * Matrix2[0][1] + Matrix1[0][1] * Matrix2[1][1];
-    OutputMatrix[1][0] = Matrix1[1][0] * Matrix2[0][0] + Matrix1[1][1] * Matrix2[1][0];
-    OutputMatrix[1][1] = Matrix1[1][0] * Matrix2[0][1] + Matrix1[1][1] * Matrix2[1][1];
+    
+    int matrix1[2][2] = {{1, 1}, {0, 1}};
+    int matrix2[2][2] = {{2, 0}, {3, 0}};
+    int outputMatrix[2][2];
+    
+    outputMatrix[0][0] = matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0];
+    outputMatrix[0][1] = matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1];
+    outputMatrix[1][0] = matrix1[1][0] * matrix2[0][0] + matrix1[1][1] * matrix2[1][0];
+    outputMatrix[1][1] = matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1];
+    
     for (int i = 0; i < 2; i++) {
-        for(int j = 0; j < 2; j++) {
-            printf("%d ", OutputMatrix[i][j]);
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", outputMatrix[i][j]);
         }
         printf("\n");
     }
