832c52146a96db431fb8cd4cfe9e0c19e19e9962
Danya Khrapovitckii <40688428+danyaffff@users.noreply.github.com>
2020-02-28 09:56:18 +0300

    Update main.c

--- Sem 1, Lab 5/main.c
+++ Sem 1, Lab 5/main.c
@@ -1,6 +1,7 @@
 /* Лабораторная работа №5. Массивы */
 
 #include <stdio.h>
+
 int main() {
     int arr[] = {12, 13, 14, 15, 16, 17, 18, 19, 20};
     for (int i = 0; i < sizeof(arr) / sizeof(int)+10; i++) {
